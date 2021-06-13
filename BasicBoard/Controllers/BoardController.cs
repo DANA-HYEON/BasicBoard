faee1a67698e952ac496b59601c9dd030fa21c2d
DANA-HYEON <dada4202@gmail.com>
2021-06-14 01:04:40 +0900

    게시물 상세보기 개발

--- BasicBoard/Controllers/BoardController.cs
+++ BasicBoard/Controllers/BoardController.cs
@@ -28,6 +28,22 @@ namespace BasicBoard.Controllers
             
         }
 
+        public IActionResult Detail(int boardNo) //게시판 상세
+        {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                //로그인이 안된 상태
+                return RedirectToAction("Login", "Account");
+            }
+
+            using(var db = new BasicboardDbContext())
+            {
+                var board = db.Board.FirstOrDefault(b => b.BoardNo.Equals(boardNo));
+                return View(board);
+            }
+
+        }
+
         public IActionResult Add() //게시물 추가
         {
             if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
