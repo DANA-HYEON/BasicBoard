077013f232f6b42a94e11f26ac9b534e6a751610
DANA-HYEON <dada4202@gmail.com>
2021-06-15 16:08:14 +0900

    게시물 작성자 이름 join으로 가져오기 / 리스트 최신순 정렬  / 검색기능 개발 완료

--- BasicBoard/Controllers/BoardController.cs
+++ BasicBoard/Controllers/BoardController.cs
@@ -1,5 +1,6 @@
 ﻿using BasicBoard.Data;
 using BasicBoard.Models;
+using BasicBoard.ViewModel;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -14,40 +15,54 @@ namespace BasicBoard.Controllers
     public class BoardController : Controller
     {
 
-        public IActionResult Index() //게시판 리스트
+        public IActionResult Index(string category, string searchString) //게시판 리스트
         {
-            if(HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
             {
                 //로그인이 안된 상태
                 return RedirectToAction("Login", "Account");
             }
 
-            using(var db = new BasicboardDbContext())
+            using (var db = new BasicboardDbContext())
             {
-                //var list = db.Board.ToList(); //Board테이블의 모든 내용을 가져오기
-                /*var list = db.Board
-                    .Join(
-                        db.User,
-                        user => user.User.UserNo,
-                        board => board.UserNo,
-                        (board, user) => new
-                        {
-                            BoardNo = board.BoardNo,
-                            BoardTitle = board.BoardTitle,
-                            BoardContent = board.BoardContent,
-                            BoardUpdateDate = board.BoardUpdateDate,
-                            BoardViews = board.BoardViews,
-                            UserName = user.UserName
-                        }
-                    ).ToList();*/
+                var list =  from b in db.Board
+                                         join u in db.User on b.UserNo equals u.UserNo
+                                         orderby b.BoardNo descending
+                                         select new BoardIndex
+                                         {
+                                             BoardNo = b.BoardNo,
+                                             BoardTitle = b.BoardTitle,
+                                             BoardContent = b.BoardContent,
+                                             BoardUpdateDate = b.BoardUpdateDate,
+                                             BoardViews = b.BoardViews,
+                                             UserName = u.UserName
+                                         };
 
-                var list = db.Board.ToList();
+                if (!String.IsNullOrEmpty(searchString) && !String.IsNullOrEmpty(category))
+                {
+                    searchString = searchString.Trim();
 
-                ViewData["test"] = list;
-                return View(list);
+                    switch (category)
+                    {
+                        case "BoardTitle":
+                            list = list.Where(s => s.BoardTitle.Contains(searchString));
+                            break;
+
+                        case "BoardContent":
+                            list = list.Where(s => s.BoardContent.Contains(searchString));
+                            break;
+
+                        case "UserName":
+                            list = list.Where(s => s.UserName.Contains(searchString));
+                            break;
+                    }
+                }
+
+                return View(list.ToList());
             }
-            
+
         }
+
 
         public IActionResult Detail(int boardNo) //게시판 상세
         {
@@ -57,7 +72,7 @@ namespace BasicBoard.Controllers
                 return RedirectToAction("Login", "Account");
             }
 
-            using(var db = new BasicboardDbContext())
+            using (var db = new BasicboardDbContext())
             {
                 var board = db.Board.FirstOrDefault(b => b.BoardNo.Equals(boardNo));
                 return View(board);
@@ -87,16 +102,15 @@ namespace BasicBoard.Controllers
             }
 
             model.UserNo = int.Parse(HttpContext.Session.GetInt32("USER_LOGIN_KEY").ToString());
-            Console.WriteLine("결과 값 : " + model);
 
             if (ModelState.IsValid)
             {
-                using(var db = new BasicboardDbContext())
+                using (var db = new BasicboardDbContext())
                 {
                     db.Board.Add(model);
                     var result = db.SaveChanges(); //Commit //성공한 갯수를 받는다.
 
-                    if(result > 0)
+                    if (result > 0)
                     {
                         return Redirect("Index"); //RedirectToAction은 컨트롤러의 입력 유무(Index가 입력된 컨트롤러 호출)
                     }
@@ -134,9 +148,9 @@ namespace BasicBoard.Controllers
                 return RedirectToAction("Login", "Account");
             }
 
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
-                
+
                 using (var db = new BasicboardDbContext())
                 {
                     var board = db.Board.FirstOrDefault(b => b.BoardNo.Equals(boardNo)); //수정하려는 게시물 정보 DB에서 가져오기
@@ -144,10 +158,10 @@ namespace BasicBoard.Controllers
                     board.BoardTitle = model.BoardTitle; //제목 수정
                     board.BoardContent = model.BoardContent; //내용 수정
 
-                    db.Update(board); 
+                    db.Update(board);
                     var result = db.SaveChanges();
 
-                    if(result > 0)
+                    if (result > 0)
                     {
                         return Redirect("Index");
                     }
@@ -166,7 +180,7 @@ namespace BasicBoard.Controllers
                 return RedirectToAction("Login", "Account");
             }
 
-            using(var db = new BasicboardDbContext())
+            using (var db = new BasicboardDbContext())
             {
                 var board = db.Board.FirstOrDefault(b => b.BoardNo.Equals(boardNo)); //삭제하려는 게시물 정보 DB에서 가져오기
                 return View(board);
@@ -174,7 +188,7 @@ namespace BasicBoard.Controllers
         }
 
 
-        [HttpPost,ActionName("Delete")]
+        [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public IActionResult DeleteConfirmed(int boardNo) //게시물 삭제
         {
@@ -188,17 +202,17 @@ namespace BasicBoard.Controllers
 
             {
                 var board = db.Board.FirstOrDefault(b => b.BoardNo.Equals(boardNo)); //삭제하려는 게시물 정보 DB에서 가져오기
-                if(board != null)
+                if (board != null)
                 {
                     db.Remove(board);
                 }
 
                 var result = db.SaveChanges();
 
-                if(result > 0)
+                if (result > 0)
                 {
                     TempData["success"] = boardNo;
-                    return RedirectToAction("Index","Board");
+                    return RedirectToAction("Index", "Board");
                 }
             }
             return View();
---
+++ BasicBoard/ViewModel/BoardIndex.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BasicBoard.ViewModel
+{
+    public class BoardIndex
+    {
+
+        
+        public int BoardNo { get; set; } //게시물 번호
+        
+        public string BoardTitle { get; set; }  //게시물 제목
+        
+        public string BoardContent { get; set; } //게시물 내용
+
+        public DateTime BoardUpdateDate { get; set; } //게시물 등록날짜
+
+        public int BoardViews { get; set; } //게시물 조회수
+
+        public string UserName { get; set; } //등록자
+    }
+}
